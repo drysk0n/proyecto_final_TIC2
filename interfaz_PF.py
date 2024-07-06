7e6a64ab075dbf0575e27d54769e8680f8c8021c
drysk0n <jmora2022@udec.cl>
2024-07-06 04:01:22 -0400

    Update interfaz_PF.py

--- interfaz_PF.py
+++ interfaz_PF.py
@@ -198,6 +198,25 @@ class Ui_MainWindow(object):
         self.ui_humedad.setupUi(self.detalle_humedad)
         self.detalle_humedad.show()
 
+#Integración de la camara en la interfaz
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+        self.cap = cv2.VideoCapture(0)  # 0 for default webcam
+        self.timer.start(30)  # Update frame every 30 milliseconds
+
+    def update_frame(self):
+        ret, frame = self.cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert frame to RGB format
+            img = QtGui.QImage(frame, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888)
+            pixmap = QtGui.QPixmap.fromImage(img)
+            self.Camara.setPixmap(pixmap.scaled(self.Camara.size(), QtCore.Qt.KeepAspectRatio))
+  
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
